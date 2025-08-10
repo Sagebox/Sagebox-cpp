2c0d4783c8c66b96a39d501cb235135e31fc5e13
Rob Nelson <robnelsonxx2@gmail.com>
2025-08-10 15:28:38 -0700

    Update WinMain.cpp

--- SageBox WinMain for Console/WinMain.cpp
+++ SageBox WinMain for Console/WinMain.cpp
@@ -169,5 +169,4 @@ int APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
     return main(iArgC,sOutArgs);    // Finally, call the C/C++ normative main() function
 
     // sArgs & vArgs get deleted here automatically
-
 }
